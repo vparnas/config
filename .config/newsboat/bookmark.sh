c341e7629054b637cdf93c87d0377cada4db7533
Vitaly Parnas <vp330@parnas.me>
2022-02-25 14:13:57 -0500

    Misc updates

--- .config/newsboat/bookmark.sh
+++ .config/newsboat/bookmark.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-# this is a simple example script that demonstrates how bookmarking plugins for newsbeuter are implemented
-# (c) 2007 Andreas Krennmair
-# (c) 2016 Alexander Batischev
+#!/bin/bash
 
 url="$1"
 title="$2"
 description="$3"
 feed_title="$4"
 
-echo -e "${feed_title}: ${title} - ${description} ${url}" >> $NOTES
+echo -e "${feed_title}: ${title}\n    ${url}" >> $NOTES
